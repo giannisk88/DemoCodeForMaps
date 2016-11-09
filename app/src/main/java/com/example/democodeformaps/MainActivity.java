6bfb523bc8370eee40235aa7cb35f65da1d75b7d
DESKTOP-PC\Giannis <giannisk88@gmail.com>
2016-11-09 12:53:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/democodeformaps/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.democodeformaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng myLocation = new LatLng(35.3454602,25.1425848);
+        mMap.addMarker(new MarkerOptions().position(myLocation).title("Heraklion Crete"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+
+    }
+}
